81d4cb5b22204489259995d1dfd9e724b0ee4b43
gabesullice <gabriel@sullice.com>
2015-04-10 19:12:31 -0600

    Add check for chruby

--- ruby/default_src.sh
+++ ruby/default_src.sh
@@ -1,9 +1,11 @@
-# Sets up chruby
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
+if command chruby -v >/dev/null 2>&1; then
+  # Sets up chruby
+  source /usr/local/opt/chruby/share/chruby/chruby.sh
+  source /usr/local/opt/chruby/share/chruby/auto.sh
 
-# Sets up gem_home
-source /usr/local/share/gem_home/gem_home.sh
+  # Sets up gem_home
+  source /usr/local/share/gem_home/gem_home.sh
+fi
 
 _bundle_install() {
   if [ -f .ruby-gemset ]; then
