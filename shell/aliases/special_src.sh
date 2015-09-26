4d9285f6586d569100d1da743714bd845cd3dc23
Gabriel SULLICE <gabriel@sullice.com>
2015-09-25 22:07:20 -0600

    Bash alias clean up

--- shell/aliases/special_src.sh
+++ shell/aliases/special_src.sh
@@ -1,29 +1,16 @@
 # These aliases not directly related to the shell, like git, vim, compass etc.
 
-# Git aliases. Note: git command aliases should go in your .gitconfig file.
-alias g="git"
-
 # Go related
 alias gohome="cd $GOPATH/src/github.com/gabesullice"
 
-# Aliases vim
-alias v="vim"
+# Aliases vi to vim
+alias vi="vim"
 
 # Creates quick alias to get to cd the dotfiles directory.
 alias dotfiles="cd $HOME/.dotfiles"
 
-# compass watch alias
-alias cw="compass watch"
-
 # Edit Hosts file
 alias vim_hosts="sudo vim /etc/hosts"
-# Edit vHosts File
-alias vim_vhosts="vim /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf"
-# Drupal alias folder
-alias vim_alias="vim ~/.drush"
-
-# MAC only
-alias dnsmasq_restart="sudo launchctl stop homebrew.mxcl.dnsmasq && sudo launchctl start homebrew.mxcl.dnsmasq"
 
 # Clone the e3 StarterKit
 alias starterkit="git clone --recursive git@github.com:elevatedthird/starterkit"
