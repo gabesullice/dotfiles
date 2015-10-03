3b28bbcecfa77ee7b01f31a132d348bab47dfef3
Gabriel SULLICE <gabriel@sullice.com>
2015-10-02 23:27:25 -0600

    Remove mcabber and irssi config

--- setup.sh
+++ setup.sh
@@ -50,9 +50,3 @@ setup_dotfile $HOME/.dotfiles/bash/bashrc $HOME/.bashrc
 
 # Tmux configuration
 setup_dotfile $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
-
-# mcabber configuration
-setup_dotfile $HOME/.dotfiles/mcabber/mcabber $HOME/.mcabber
-
-# irssi configuration
-setup_dotfile $HOME/.dotfiles/irssi/irssi $HOME/.irssi
