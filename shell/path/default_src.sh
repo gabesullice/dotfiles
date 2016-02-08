575851b6717c558b6f35aa490f738da35607d1b3
Gabriel SULLICE <gabriel@sullice.com>
2016-02-08 15:13:44 -0700

    Update submodules

--- shell/path/default_src.sh
+++ shell/path/default_src.sh
@@ -1,6 +1,10 @@
 # Go
+export GO15VENDOREXPERIMENT=1
 export GOPATH=$PROJECTROOT/go
 export PATH=$PATH:$GOPATH/bin
+
+# Ruby
+export PATH=$PATH:$HOME/.gem/ruby/2.3.0/bin
 
 # User bin
 export PATH=$PATH:$HOME/bin
