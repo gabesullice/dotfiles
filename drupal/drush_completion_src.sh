5bbd1723fec480e63a6f6d46f692dacb780664c5
Gabriel SULLICE <gabriel@sullice.com>
2016-02-24 09:32:29 -0700

    Add drush completions

--- drupal/drush_completion_src.sh
+++ drupal/drush_completion_src.sh
@@ -1,1 +1,11 @@
 source $HOME/.dotfiles/drupal/drush/drush.complete.sh
+
+# Include Drush completion.
+if [ -f "/home/gabe/.drush/drush.complete.sh" ] ; then
+  source /home/gabe/.drush/drush.complete.sh
+fi
+
+# Include Drush prompt customizations.
+if [ -f "/home/gabe/.drush/drush.prompt.sh" ] ; then
+  source /home/gabe/.drush/drush.prompt.sh
+fi
